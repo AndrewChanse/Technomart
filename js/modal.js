fc0dd902edae2d5871a99157a0a653c515a9e1b7
Andrew <andryccio@gmail.com>
2017-12-05 23:31:05 +0200

    js for modal almost done

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,9 @@
 var contact = document.querySelector(".aulost-btn");
-var modal = document.querySelector(".contact-form");
+var modal = document.querySelector(".modal-window");
 var close = modal.querySelector("[name=exit-btn]");
 var login = modal.querySelector("[name=login-field]");
-var form = document.querySelector(".contact-form")
+var email = modal.querySelector("[name=email-field]");
+var form = document.querySelector(".contact-form");
 
 contact.addEventListener("click", function(event) {
 	event.preventDefault();
@@ -12,7 +13,9 @@ contact.addEventListener("click", function(event) {
 
 form.addEventListener("submit", function(event) {
 	event.preventDefault();
-})
+	console.log(login.value);
+	console.log(email.value);
+});
 
 close.addEventListener("click", function(event) {
 	event.preventDefault();
