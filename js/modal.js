436edda58c34ca898e25f949c66ae4a0f0de2af4
Andrew <andryccio@gmail.com>
2017-12-06 22:13:42 +0200

    js for modal almost done

--- js/modal.js
+++ js/modal.js
@@ -4,20 +4,42 @@ var close = modal.querySelector("[name=exit-btn]");
 var login = modal.querySelector("[name=login-field]");
 var email = modal.querySelector("[name=email-field]");
 var form = document.querySelector(".contact-form");
+var storage = localStorage.getItem("login");
 
 contact.addEventListener("click", function(event) {
 	event.preventDefault();
 	modal.classList.add("form-view");
-	login.focus();
+if (storage) {
+	login.value = storage;
+	email.focus();
+	}
+	else {
+	login.focus();	
+	}
 });
 
 form.addEventListener("submit", function(event) {
-	event.preventDefault();
-	console.log(login.value);
-	console.log(email.value);
+	if (!login.value || !email.value) {
+		event.preventDefault();
+		console.log ("Нужно ввести логин и пароль!");
+	}
+	else {
+		event.preventDefault();
+		localStorage.setItem("login", login.value);
+	}
+	
 });
 
 close.addEventListener("click", function(event) {
 	event.preventDefault();
 	modal.classList.remove("form-view");
+});
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (modal.classList.contains("form-view")) {
+	modal.classList.remove("form-view");
+	}
+}
+
 });
